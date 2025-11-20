a90c95db3111b28eb697d8a732e56e7ab4277bc5
Myssal <143514975+myssal@users.noreply.github.com>
2025-11-21 00:14:55 +0700

    fix: remove redundant slash

--- main.py
+++ main.py
@@ -61,12 +61,12 @@ CONFIGS = {
     },
 
     "CN_BETA": {
-        "cdn": "http://pre-zspns-volccdn.kurogame.com/pre/",
+        "cdn": "http://pre-zspns-volccdn.kurogame.com/pre",
         "appId": "com.kurogame.haru.pioneer",
         "platform": "android",
     },
     "CN_PC_BETA": {
-        "cdn": "http://pre-zspns-volccdn.kurogame.com/pre/",
+        "cdn": "http://pre-zspns-volccdn.kurogame.com/pre",
         "appId": "com.kurogame.haru.pioneer",
         "platform": "standalone",
     },
